dda2d2ec2ac9901a9db4747d7a43675aa33dc006
jason12360 <jason12360@gmail.com>
2015-12-04 01:05:38 -0500

    fix some bugs

---
+++ carcassonne_031/src/code/GameboardView.java
@@ -0,0 +1,33 @@
+package code;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+ /**
+  * This class used to draw background image in the JPanel
+  * @author Administrator
+  *
+  */
+public class GameboardView extends JPanel{
+	
+	public void paintComponent(Graphics g){
+		int x = 0, y = 0;
+		java.net.URL imgURL = getClass().getResource("/resources/CarcassonneGameBoard.png");
+		ImageIcon icon = new ImageIcon(imgURL);
+		g.drawImage(icon.getImage(), 0, 0, getSize().width,getSize().height,this);
+		while(true){
+			g.drawImage(icon.getImage(), x, y, this);
+			if(x>getSize().width && y > getSize().height)break;
+			if(x>getSize().width){
+				x = 0;
+				y += icon.getIconHeight();
+			}
+			else
+				x+=icon.getIconWidth();
+		}
+		
+	}
+}
